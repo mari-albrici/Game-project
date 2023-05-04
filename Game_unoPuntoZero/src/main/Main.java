691e29b143c2c1a8838378ae20114671efb255df
AndreaBar94 <andreabarocchi@gmail.com>
2023-05-04 15:20:49 +0200

    first commit

---
+++ Game_unoPuntoZero/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Guarda quanto son bellino");
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+
+	}
+
+}
