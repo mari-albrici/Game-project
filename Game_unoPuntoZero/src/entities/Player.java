7bd5164d666097725db6f80fef32ef274aadcab7
AndreaBar94 <andreabarocchi@gmail.com>
2023-05-05 14:25:47 +0200

    she moooves with her legs

--- Game_unoPuntoZero/src/entities/Player.java
+++ Game_unoPuntoZero/src/entities/Player.java
@@ -67,6 +67,17 @@ public class Player extends Entity{
 			direction = "right";
 			x += speed;
 		}
+		
+		spriteCounter++;
+		if(spriteCounter > 10) {
+			if(spriteNum == 1) {
+				spriteNum = 2;
+			}
+			else if(spriteNum == 2) {
+				spriteNum = 1;
+			}
+			spriteCounter = 0;
+		}
 	}
 	
 	public void draw(Graphics2D g2) {
@@ -78,19 +89,39 @@ public class Player extends Entity{
 		
 		switch(direction) {
 			case "up":
-				image = up1;
+				if(spriteNum == 1) {
+					image = up1;
+				}
+				if(spriteNum == 2) {
+					image = up2;
+				}
 				break;
 				
 			case "down":
-				image = down1;
+				if(spriteNum == 1) {
+					image = down1;
+				}
+				if(spriteNum == 2) {
+					image = down2;
+				}
 				break;
 				
 			case "left":
-				image = left1;
+				if(spriteNum == 1) {
+					image = left1;
+				}
+				if(spriteNum == 2) {
+					image = left2;
+				}
 				break;
 				
 			case "right":
-				image = right1;
+				if(spriteNum == 1) {
+					image = right1;
+				}
+				if(spriteNum == 2) {
+					image = right2;
+				}
 				break;
 		}
 		
